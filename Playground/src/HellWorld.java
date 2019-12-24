b904312c27e86e40a7806a74122c6d9945bed7cd
Harry <harrylhp2002@yahoo.com>
2019-12-24 21:14:45 +0800

    Update HellWorld.java

--- Playground/src/HellWorld.java
+++ Playground/src/HellWorld.java
@@ -3,6 +3,7 @@ public class HellWorld {
 	public static void main(String[]args) {
 		System.out.println("Hello World");
 		System.out.println("This is the testing");
+		System.out.println("The program is added into GitHub");
 	}
 	
 
