cba6ea8d304c8225935386d56fc192859d394ef8
harrylhp <harrylhp2002@yahoo.com>
2020-12-21 12:00:33 +0800

    Update code

--- Playground/src/HashTable/Challenge/findFirstOccurrence.java
+++ Playground/src/HashTable/Challenge/findFirstOccurrence.java
@@ -1,14 +1,18 @@
 package HashTable.Challenge;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.MultiHashtable;
+
 import java.util.HashSet;
+import java.util.Hashtable;
 
 public class findFirstOccurrence {
     public static void main (String[]args) {
         String str = "Har hehe hoho";
         char [] arrayChar = str.toCharArray();
+        int i;
 
         HashSet <Character> hSet = new HashSet<>();
-        for (int i = 0; i< arrayChar.length; i++){
+        for (i = 0; i< arrayChar.length; i++){
             if(hSet.contains(arrayChar[i])){
                 System.out.println("Char is " + arrayChar[i]);
                 break;
@@ -18,6 +22,18 @@ public class findFirstOccurrence {
         }
 
         System.out.print(hSet + " ");
+        System.out.println();
+
+        Hashtable <Character, Integer> hTable = new Hashtable<>();
+        for (i=0; i<arrayChar.length; i++){
+            if (hTable.containsKey(arrayChar[i])){
+                System.out.println("Char is " + arrayChar[i]);
+                break;
+            }
+            else
+                hTable.put(arrayChar[i],i);
+        }
+        System.out.print(hTable + " ");
     }
 
 }
